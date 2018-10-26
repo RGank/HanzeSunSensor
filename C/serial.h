9896c5cf81ca7a2fbc8432e0094d6697b543efa2
Arnold217 <arnoldbuining@hotmail.com>
2018-10-26 13:43:52 +0200

    Seperate into different files for readability

---
+++ C/serial.h
@@ -0,0 +1,39 @@
+void uart_init()
+{
+	//Set baud rate
+	UBRR0H = 0;
+	UBRR0L = UBBRVAL;
+	
+	//Disable U2X mode
+	UCSR0A = 0;
+	
+	//Enable transmitter and receiver
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	
+	//Set frame format : asynchronous, 8 data bits, 1 stop bit, no parity
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+}
+
+/*
+	Transmit data to the client
+	Reference: ATMEGA328p Datasheet 20.6.1
+*/
+void transmit(unsigned char data){
+	/* Wait for empty transmit buffer */
+	while( !(UCSR0A & (1<<UDRE0)) );
+	
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
+
+/*
+	Receive input from the client
+	Reference: ATMEGA328p Datasheet 20.7.1
+*/
+unsigned char receive( void )
+{
+	/* Wait for data to be received */
+	while ( !(UCSR0A & (1<<RXC0)) );
+	/* Get and return received data from buffer */
+	return UDR0;
+}
--- C/zonnescherm.c
+++ C/zonnescherm.c
@@ -10,50 +10,9 @@
 #include <avr/sfr_defs.h>
 #define F_CPU 16E6
 #include <util/delay.h>
-
-//Output on USB = PD1 = Board pin 1
-//Datasheet p.190; F_OSC = 16 MHz & baud rate = 19.200
 #define UBBRVAL 51
 
-void uart_init()
-{
-	//Set baud rate
-	UBRR0H = 0;
-	UBRR0L = UBBRVAL;
-	
-	//Disable U2X mode
-	UCSR0A = 0;
-	
-	//Enable transmitter and receiver
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	
-	//Set frame format : asynchronous, 8 data bits, 1 stop bit, no parity
-	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
-}
-
-/*
-	Transmit data to the client
-	Reference: ATMEGA328p Datasheet 20.6.1
-*/
-void transmit(unsigned char data){
-	/* Wait for empty transmit buffer */
-	while( !(UCSR0A & (1<<UDRE0)) );
-	
-	/* Put data into buffer, sends the data */
-	UDR0 = data;
-}
-
-/*
-	Receive input from the client
-	Reference: ATMEGA328p Datasheet 20.7.1
-*/
-unsigned char receive( void )
-{
-	/* Wait for data to be received */
-	while ( !(UCSR0A & (1<<RXC0)) );
-	/* Get and return received data from buffer */
-	return UDR0;
-}
+#include "serial.h"
 
 /*
 	Connection has been lost, wait for a new connection to be made.
