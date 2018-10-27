3529a5235ed9fa86fdfa4983876a94f9cfc08a63
DLatumalea <d.latumalea@hotmail.com>
2018-10-27 13:04:41 +0200

    Add files via upload

--- Python/dash test/myproject/server.py
+++ Python/dash test/myproject/server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from dash import Dash
 
-server = Flask('myproject')
+server = Flask(__name__, root_path='/myproject')
 app = Dash(server=server)
 app.config['suppress_callback_exceptions']=True
