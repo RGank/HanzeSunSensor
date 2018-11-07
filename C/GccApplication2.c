6758a0a2334defb070dc6da97c38e935151c7a33
Rein Gankema <p.r.gankema@st.hanze.nl>
2018-11-07 02:18:21 +0100

    Work in progress
    updated comments + interrupts

--- C/GccApplication2.c
+++ C/GccApplication2.c
@@ -27,65 +27,89 @@
 #include "distance.h"
 #include "serial.h"
 
-volatile uint8_t gv_echo = 0; // a flag
-volatile uint8_t gv_count = 0;
+static volatile uint8_t gv_echo = 0; // a 16 bit flag
+static volatile uint8_t gv_count = 0;
 
-uint16_t distance = 0;
+uint16_t distance;
+int working = 0;
+
 
 void init_ports(void)
 {
-	DDRD = 0b11110111;
-	
+	DDRD = 0b11110111;			//Set TRIG pin on output (PIN 0) and set ECHO pin on input (PIN 3)
+	_delay_ms(50);
+
 }
 
-void init_timer(void)
+void Initialize_external_interrupt(void)
+{
+	EIMSK |= (1 << INT0);	// enable INT0
+	EICRA |= (1 << ISC10);	// set INT0 to trigger while rising edge = HIGH
+}
+
+void init_timer()
 // prescaling : max time = 2^16/16E6 = 4.1 ms, 4.1 >> 2.3, so no prescaling required
 // normal mode, no prescale, stop timer
 {
-    TCCR1B = 0;
+    TCCR1A = 0;
+	TCCR1B = 0;
 }
 
 uint16_t Pulse()
 {
-	 PORTD |=(1<<PIND0);
-	 _delay_us(50);
-	 PORTD &=~(1<<PIND0);
-	
-	distance = gv_echo / 58;		// Zet afstand om van ms naar cm
-	
-	return distance;
+		_delay_ms(10);			//Restart HC-SR04
+		PORTD &= ~(1 << PIND0);
+		_delay_us(1);
+		PORTD |=(1<<PIND0);	//Set PIN 0 HIGH
+		_delay_us(10);
+		PORTD &= ~(1<<PIND0); //Set PIN 0 LOW		
 }	
 
-ISR (INT1_vect)
+uint16_t calc_cm()
 {
-	if(gv_count == 1)
-	{
-		TCCR1B = 0;				//disable counter
-		gv_echo = TCNT1;		//update counter to integer
-		TCNT1 = 0;				//reset counter
-		gv_count = 0;
-		
-	}
-	if(gv_count == 0)
-	{
-		TCCR1B = _BV(CS10);		//enable counter
-		gv_count = 1;
-	}
+	distance = gv_echo / 58;		//Convert the distance in us to cm
 }
 
 
 int main(void)
 {
-	init_timer();
-	init_ports();
-	uart_init();			//Init UART
+	init_timer();						//Init timer
+	init_ports();						//Init ports
+	Initialize_external_interrupt();	//Init interrupts
+	uart_init();						//Init UART
 	stdout = &mystdout;
-	sei();					//enable interrupssssssssssssssssssssssssssss
+	sei();								//enable interrupts
 	
 	while (1)
 	{
-		INT1_vect();
 		Pulse();
+		calc_cm();
 		printf("distance %d \r\n: ", distance);
 	}
+}
+
+ISR (INT0_vect)
+{
+	if (gv_count==1)//when logic from HIGH to LOW
+	{
+			TCCR1B=0;//disabling counter
+
+			gv_echo=TCNT1;//count memory is updated to integer
+			
+			TCNT1=0;//resetting the counter memory
+			
+			gv_count=0;
+			
+		}
+
+		if (gv_count==0)//when logic change from LOW to HIGH
+
+		{
+
+			TCCR1B|=(1<<CS10);//enabling counter
+
+			gv_count=1;
+			
+		}
+	
 }
